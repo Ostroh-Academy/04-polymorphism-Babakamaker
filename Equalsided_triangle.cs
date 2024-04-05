8a35a809a53b336b74904683e2ac59850033ceaf
Viktoriia Babaieva <145108530+Babakamaker@users.noreply.github.com>
2024-04-05 23:57:07 +0300

    Add files via upload

---
+++ Equalsided_triangle.cs
@@ -0,0 +1,33 @@
+﻿namespace Triangles
+{
+    public class EquilateralTriangle
+    {
+        public double sideLength;
+        public double angle;
+
+        public virtual void SetValues(double sideLength, double angle)
+        {
+            this.sideLength = sideLength;
+            this.angle = angle;
+        }
+
+        public virtual double[] GetSideLengths()
+        {
+            return new double[] { sideLength, sideLength, sideLength };
+        }
+
+        public virtual double CalculatePerimeter()
+        {
+            return 3 * sideLength;
+        }
+        public virtual string GetTriangleType()
+        {
+            return "Equilateral";
+        }
+
+        public virtual string GetDescription()
+        {
+            return $"This is an {GetTriangleType()} triangle with side length {sideLength} and angle {angle} degrees.";
+        }
+    }
+}
